15d544ded5ff1c62261638fc6762e87dbed4c6db
dev <a.s.elliethy@gmail.com>
2022-04-25 00:32:18 +0200

    update

--- lib/api/request/refund_request_body.dart
+++ lib/api/request/refund_request_body.dart
@@ -5,7 +5,7 @@ import 'package:geideapay/api/request/base/post_pay_operation_request_body.dart'
 import 'package:geideapay/common/card_utils.dart';
 
 class RefundRequestBody extends PostPayOperationRequestBody {
-  String? callbackUrl1, reason1;
-  RefundRequestBody(String orderId, {this.callbackUrl1, this.reason1}):
-        super(orderId, callbackUrl: callbackUrl1, reason: reason1);
+  String? callbackUrl_, reason_;
+  RefundRequestBody(String orderId, {this.callbackUrl_, this.reason_}):
+        super(orderId, callbackUrl: callbackUrl_, reason: reason_);
 }
